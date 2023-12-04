a7600a05f19cdec290e8227e91d3cf8211acb472
Wew_K <Ketpicpred@myvuw.ac.nz>
2023-12-05 02:38:08 +0700

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,4 +1,4 @@
-//this is orange cat
+//this is gray cat and different eyes color
 //ear
 let earTopY = 10 //the pointy ear
 let earOutY = 60 //all 
@@ -7,7 +7,7 @@ let earRightX = 155 //right ear back&front out&top is X
 let earInY = 40 //the back one
 let earIn2Y = 45 //the front one
 //eye
-let eyesOpen = true //if statement or condition
+let openRightEye = true //if statement or condition. No. 77
 let eyeScale = 30
 let eyeScaleIn = 25 //the inside eyes
 let eyeMoveY = 70 //move both eyes
@@ -27,40 +27,46 @@ let lineOut3Y = 120 //both L&R
 let mouthDownY = 110
 let mouthTopY = 102
 let mouthTopX = 102
-//line on body
-let lineDownY = 50
 //colors
 let white = 255
 let black = 0
-//let pink = color(255,192,203);//No. 55
-//let green = color(173,255,0);//No. 56
-//let cream = color(255,156,89);//No. 57
-//let orange = color(255,88,15);//No. 58
+//let pink = color(255,192,203);//No. 57
+//let gray = color(167,173,186);/No. 58
+//let blue = color(0,236,255);/No. 59
+//let yellow = color(255, 193, 0);//No. 60
+//let eyeColor = blue//No. 63
+//let bodyColor = gray//No. 64
+//let leftEyecolor = yellow//No. 65
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GLIDE_WALLPAPER);
-  pWallpaper.resolution(NINE_PORTRAIT);
+  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.resolution(A3);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width = 200;
-  pWallpaper.grid_settings.cell_height = 260;
+  pWallpaper.grid_settings.cell_height = 250;
   pWallpaper.grid_settings.row_offset = 130;
 }
 
 function wallpaper_background() {
-  background(218, 245, 241); //really light sky blue
+  background(255, 249, 249); //white with little bit of pink
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   let pink = color(255, 192, 203);
-  let green = color(173, 255, 0);
-  let cream = color(255, 156, 89);
-  let orange = color(255, 88, 15);
+  let gray = color(167, 173, 186);
+  let blue = color(0, 236, 255);
+  let yellow = color(255, 193, 0);
+
+  //body color
+  let eyeColor = blue
+  let leftEyecolor = yellow
+  let bodyColor = gray
 
   //Ears
-  fill(cream); //back ear
-  stroke(cream);
+  fill(bodyColor); //back ear
+  stroke(bodyColor);
   triangle(earLeftX, earOutY, earLeftX, earTopY, 95, earInY); //leaf
   triangle(110, earInY, earRightX, earTopY, earRightX, earOutY); //right
   fill(pink); //front ear
@@ -68,27 +74,31 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   triangle(earLeftX, earOutY, earLeftX, earTopY, 85, earIn2Y); //front ear left
   triangle(120, earIn2Y, earRightX, earTopY, earRightX, earOutY); //front ear rigth
   //Face
-  stroke(cream);
-  fill(cream);
+  stroke(bodyColor);
+  fill(bodyColor);
   ellipse(100, 85, 125, 110);
   //Eyes
-  if (eyesOpen) {
-    fill(green);
-    stroke(green);
+  if (openRightEye) {
+    fill(leftEyecolor);
+    stroke(leftEyecolor);
     ellipse(78, eyeMoveY, eyeScale); //left
     fill(black);
     ellipse(79, eyeMoveY, eyeScaleIn); //inside eye
-    fill(green);
+    fill(eyeColor);
     ellipse(125, eyeMoveY, eyeScale); //right
     fill(black);
     ellipse(124, eyeMoveY, eyeScaleIn); //inside eye
   } else {
-    //sleep eyes
+    fill(eyeColor);
+    stroke(eyeColor);
+    ellipse(78, eyeMoveY, eyeScale); //left
+    fill(black);
+    ellipse(79, eyeMoveY, eyeScaleIn); //inside eye
     strokeWeight(1.5);
     stroke(black);
-    line(60, 75, 85, 75); //left
-    line(115, 75, 140, 75); //right}
+    line(115, 75, 140, 75);
   }
+
   //Nose
   fill(pink);
   strokeWeight(0.5);
@@ -104,12 +114,20 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   line(lineInRightX, lineIn2Y, lineOutRightX, lineIn2Y); //line 2 right
   line(lineOutRightX, lineOut3Y, lineInRightX, lineIn3Y); //line 3 right
   //Mouth
+  stroke(black);
   line(mouthTopX, mouthTopY, 95, mouthDownY); //left
   line(110, mouthDownY, mouthTopX, mouthTopY); //right
-  //line in body
-  strokeWeight(5);
-  stroke(orange);
-  line(92, lineDownY, 85, 34); //left
-  line(101, lineDownY, 101, 32); //middle
-  line(110, lineDownY, 117, 34); //right
+
+  fill(yellow);
+  noStroke();
+  beginShape();
+  vertex(-10, 10);
+  vertex(0, 35);
+  vertex(10, 10);
+  vertex(35, 0);
+  vertex(10, -8);
+  vertex(0, -35);
+  vertex(-10, -8);
+  vertex(-35, 0);
+  endShape();
 }
